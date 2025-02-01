9d20633e64b2c4cec809a04e67ca67f624432ebd
Rangelzin <victorrangeltasse85@hotmai.com>
2025-02-01 17:51:10 -0300

    Resolucao ex017

--- ExercíciosJS/script/javascript17.js
+++ ExercíciosJS/script/javascript17.js
@@ -1,13 +1,19 @@
 function contar(){
-    var inicio = document.getElementById('init').value
-    var fim = document.getElementById('iend').value
-    var passo = document.getElementById('ipass').value
+    var inicio = document.getElementById('init')
+    var fim = document.getElementById('iend')
+    var passo = document.getElementById('ipass')
     var res = document.getElementById('res')
+    
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        alert('[ERRO] Faltam dados!')
+    } else {
+        res.innerHTML = 'Contando:' 
+        var i = Number(inicio.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
 
-    res.innerHTML = `<p>Contando:</p>` 
-
-    do{
-        res.innerHTML = `${inicio}`
-        inicio = inicio + passo
-    }while (inicio <= fim);
+        for(var c = i; c <= f; c += p){
+            res.innerHTML += `${c} \u{1F603}`
+        }
+    }   
 } 
---
+++ ExercíciosJS/script/javascript18.js
@@ -0,0 +1,13 @@
+function tabuada() {
+    var num = document.getElementById('inum').value
+    var tab = document.createElement('p')
+    var tabuada = document.getElementById('tabuada')
+    var c = 1
+
+    while (c <= 10){
+        
+    }
+
+    tabuada.innerHTML = `Seu numero e ${num}`
+
+}
