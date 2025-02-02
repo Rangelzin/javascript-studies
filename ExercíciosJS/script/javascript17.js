585d4d7aeab2822eeb0a452b50683b6cc600fda7
Rangelzin <victorrangeltasse85@hotmai.com>
2025-02-02 01:44:39 -0300

    Conclusão dos exercício 17 e do 18

--- ExercíciosJS/script/javascript17.js
+++ ExercíciosJS/script/javascript17.js
@@ -1,19 +1,44 @@
-function contar(){
-    var inicio = document.getElementById('init')
-    var fim = document.getElementById('iend')
-    var passo = document.getElementById('ipass')
-    var res = document.getElementById('res')
-    
-    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
-        alert('[ERRO] Faltam dados!')
-    } else {
-        res.innerHTML = 'Contando:' 
-        var i = Number(inicio.value)
-        var f = Number(fim.value)
-        var p = Number(passo.value)
+function contar() {
+    let inicio = document.getElementById('ni')
+    let fim = document.getElementById('nf')
+    let passo = document.getElementById('np')
+    let res = document.getElementById('res')
 
-        for(var c = i; c <= f; c += p){
-            res.innerHTML += `${c} \u{1F603}`
+    if ( inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        alert('[ERRO] Dados insuficientes!')
+        res.innerHTML = 'Impossível contar!'
+    } else {
+        res.innerHTML = 'Contando... </br>'
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        if ( p <= 0 ){
+            alert('Passo inválido!')
         }
-    }   
-} 
+
+
+        if (i < f) {
+            for(let c = i ; c <= f ; c += p ){
+                res.innerHTML += `${c}\u{1F4CC} `
+            }
+        } else {
+            for(let c = i ; c >= f ; c -= p ){
+                res.innerHTML += `${c}\u{1F4CC} `
+            }
+        }
+    }
+}
+
+
+// USOS EQUIVALENTES
+
+/*
+    let c = i
+
+    res.innerHTML += `${c} `
+    while (c <= f){
+        c += p
+        res.innerHTML += `${c} `
+    }
+*/  
--- ExercíciosJS/script/javascript18.js
+++ ExercíciosJS/script/javascript18.js
@@ -1,13 +1,19 @@
 function tabuada() {
-    var num = document.getElementById('inum').value
-    var tab = document.createElement('p')
+    var num = document.getElementById('inum')
     var tabuada = document.getElementById('tabuada')
-    var c = 1
 
-    while (c <= 10){
-        
-    }
-
-    tabuada.innerHTML = `Seu numero e ${num}`
-
+    if (num.value.length == 0){
+        alert('Digite novamente!')
+    } else {
+        var n = (num.value)
+        var c = 1
+        tabuada.innerHTML = ''
+        while (c <= 10){
+            var item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tabuada${c}`
+            tabuada.appendChild(item)
+            c++
+        }
+    } 
 }
