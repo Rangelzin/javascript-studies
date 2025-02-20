3d9e40fcfb11143a88d755dd66b24fb20e830296
Rangelzin <victorrangeltasse85@hotmai.com>
2025-02-20 15:01:04 -0300

    Novo modulo e novos conteudos

---
+++ Aprofundamento/módulo 3 - Web/scripts/2.selecionandoElementos.js
@@ -0,0 +1,31 @@
+/*
+    - Selecionando Elementos:
+
+        - getElementById() // Traz apenas um elemento (element)
+        - getElementByClassName() // Traz diversos elementos (HTML Collection)
+        - getElementByTagName() // Traz diversos elementos (HTML Collection)
+        - getElementByName() // Traz os elementos com o nome selecionado (NodeList)
+
+
+        - querySelector() // Traz apenas um elemento, o primeiro que encontrar (element)
+        - querySelectorAll() // Traz TODOS os elementos que encontrar (NodeList)
+
+    - NodeList x HTML Collection
+
+*/
+
+const element = document.getElementById("main-text")
+
+const element2 = document.getElementsByClassName("paragraph")
+
+const element3 = document.getElementsByTagName("p")
+
+const element4 = document.querySelector("p")
+
+const element5 = document.querySelectorAll("p")
+
+element5.forEach(element => {
+    console.log(element)
+});
+
+const element6 = document.getElementByName("my-input")
